3b801094d504b41137eea45d4201ccc4afac642f
lochleaf <200536258@student.georgianc.on.ca>
2026-07-19 23:21:04 -0400

    test2

--- assignment2/views/pizzaform.view.php
+++ assignment2/views/pizzaform.view.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Assignment 2 Pizza Form</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/stylesa2.css">
 </head>
 <body>
     <p>hello</p>
