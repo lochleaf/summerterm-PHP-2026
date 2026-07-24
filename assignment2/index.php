a2a718bf81ee9963fd0d5c76a3ba655cdc2454a8
lochleaf <200536258@student.georgianc.on.ca>
2026-07-23 21:51:00 -0400

    test of text
    test of text-align to change the position

--- assignment2/index.php
+++ assignment2/index.php
@@ -2,16 +2,17 @@
 
 $page = $_GET["page"] ?? "pizza";
 
-if ($page == "pizza") {
-    require "views/pizzaform.view.php";
+if ($page == "pizza") 
+    {require "views/pizzaform.view.php";
 }
 
-elseif ($page == "location") {
-    require "views/location.view.php";
+elseif ($page == "location") 
+    {require "views/location.view.php";
 }
 
-elseif ($page == "cart") {
-    require "views/cart.view.php";
+elseif ($page == "cart") 
+    {require "views/cart.view.php";
 }
 
 ?>
+
