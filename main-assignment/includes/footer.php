a3af908f0d3037ff513d7e5439ef22e02ea972ac
lochleaf <200536258@student.georgianc.on.ca>
2026-08-05 16:35:27 -0400

    starting added
    I started adding doctype in the header and footer for now and connecting it to the index.php

---
+++ main-assignment/includes/footer.php
@@ -0,0 +1,8 @@
+    <!-- my footer with just date and text -->
+    </main>
+
+    <footer>
+        <p>&copy; <?= date('Y'); ?> Cool Stuff. All rights reserved products.</p>
+    </footer>
+</body>
+</html>
---
+++ main-assignment/index.php
@@ -0,0 +1,10 @@
+<!-- include the header template that will connect the csst o the index -->
+<?php include_once 'includes/header.php'; ?>
+
+<body>
+
+    <main>
+
+    
+<!-- includes the footer -->
+<?php include_once 'templates/footer.php'; ?>
